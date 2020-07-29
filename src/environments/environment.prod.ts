5c85971152e4f49c63d5b778c6b53fb9d988d1f6
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-07-29 18:35:39 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAaDg6ZeBJVdl5g1ndBIbd0Sohp9Xhjit0",
-    authDomain: "project-lata.firebaseapp.com",
-    databaseURL: "https://project-lata.firebaseio.com",
-    projectId: "project-lata",
-    storageBucket: "project-lata.appspot.com",
-    messagingSenderId: "116703216397",
-    appId: "1:116703216397:web:f194c04183e3a070006644",
-    measurementId: "G-BRLPMBYVCX"
+    apiKey: "firebase-api-key",
+    authDomain: "firebase-project.firebaseapp.com",
+    databaseURL: "https://firebase-project.firebaseio.com",
+    projectId: "firebase-project-id",
+    storageBucket: "firebase-project.appspot.com",
+    messagingSenderId: "firebase-project-sender-id",
+    appId: "firebase-project-app-id",
+    measurementId: "firebase-project-measurement-id"
   }
 };
