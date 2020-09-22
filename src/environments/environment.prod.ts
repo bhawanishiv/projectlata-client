9039609fc70826a2a205d49e04b7f392e15e38d5
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-09-22 12:21:23 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "firebase-api-key",
-    authDomain: "firebase-project.firebaseapp.com",
-    databaseURL: "https://firebase-project.firebaseio.com",
-    projectId: "firebase-project-id",
-    storageBucket: "firebase-project.appspot.com",
-    messagingSenderId: "firebase-project-sender-id",
-    appId: "firebase-project-app-id",
-    measurementId: "firebase-project-measurement-id"
-  }
+  firebaseConfig: { }
 };
